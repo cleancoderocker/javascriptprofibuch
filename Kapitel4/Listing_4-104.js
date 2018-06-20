1ef2472d3358ef3e88b0c6d9f37b22c24982b3cb
Philip Ackermann <info@philipackermann.de>
2018-06-20 11:14:19 +0200

    Updated Listing 4.104

--- Kapitel4/Listing_4-104.js
+++ Kapitel4/Listing_4-104.js
@@ -1,13 +1,13 @@
 const name = 'Max Mustermann';
-let message = tagFunction'Mein Name ist ${name}, ich bin ${44} Jahre jung';
+let message = tagFunction`Mein Name ist ${name}, ich bin ${44} Jahre jung`;
 console.log(message); // Mein Name ist Max Mustermann, ich bin 44 Jahre jung
 message = tagFunction`Mein Name ist ${name}, ich bin ${88} Jahre jung`;
 console.log(message); // Mein Name ist Max Mustermann
 function tagFunction(strings, ...values) {
-let name = values[0];
-let age = values[1];
-if(age> 80) {
-return `${strings[0]}${values[0]}`;
-}
-return `${strings[0]}${name}${strings[1]}${age}${strings[2]}`;
+  let name = values[0];
+  let age = values[1];
+  if (age > 80) {
+    return `${strings[0]}${values[0]}`;
+  }
+  return `${strings[0]}${name}${strings[1]}${age}${strings[2]}`;
 }
