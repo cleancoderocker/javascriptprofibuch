ea5ac1f196c686fb753638f0c458ecb14ccc521c
Philip Ackermann <info@philipackermann.de>
2022-01-06 11:43:34 +0100

    Add missing function to listing 4.40

--- Kapitel4/Listing_4-40.js
+++ Kapitel4/Listing_4-40.js
@@ -1,11 +1,32 @@
-const person = createPerson(
-  (firstName = 'Max'),
-  (lastName = 'Mustermann'),
-  (height = 180),
-  (weight = 79),
-  (dayOfBirth = 2),
-  (monthOfBirth = 4),
-  (yearOfBirth = 1956)
-);
+function createPerson(
+  firstName,
+  lastName,
+  height,
+  weight,
+  dayOfBirth,
+  monthOfBirth,
+  yearOfBirth
+) {
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    height: height,
+    weight: weight,
+    dayOfBirth: dayOfBirth,
+    monthOfBirth: monthOfBirth,
+    yearOfBirth: yearOfBirth
+  };
+}
 
-const person = createPerson((yearOfBirth = 1979));
+const person = createPerson(yearOfBirth = 1979);
+console.log(person);
+
+// {
+//   firstName: 1979,
+//   lastName: undefined,
+//   height: undefined,
+//   weight: undefined,
+//   dayOfBirth: undefined,
+//   monthOfBirth: undefined,
+//   yearOfBirth: undefined
+// }
